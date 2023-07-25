fd5d91a2917b46f8853eb2f5cc3ec4d2afbbcc9f
anikaaurony <aurony111@gmail.com>
2023-07-25 18:34:02 +0600

    done commit

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
         res.send(result)
     })
 
+    app.delete('/script/:id', async (req, res) => {
+        const id = req.params.id;           
+        const query = { _id: new ObjectId(id) }   
+        const result = await scriptCollection.deleteOne(query);
+        res.send(result);
+      })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
